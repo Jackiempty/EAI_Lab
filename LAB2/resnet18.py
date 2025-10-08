d38ffdaa36b216af932cc183da6cccd6492c6444
Jackiempty <candypaper0123@gmail.com>
2025-10-08 13:35:07 +0800

    chore: commit before branching

--- LAB2/resnet18.py
+++ LAB2/resnet18.py
@@ -50,15 +50,14 @@ class ResNet(nn.Module):
         
     #This function is primarily used to repeat the same residual block
     def make_layer(self, block, channels, num_blocks, stride):
-    
 
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
+        def forward(self, x):
+            out = self.conv1(x)
+            out = self.layer1(out)
+            out = self.layer2(out)
+            out = self.layer3(out)
+            out = self.layer4(out)
+            out = F.avg_pool2d(out, 4)
+            out = out.view(out.size(0), -1)
+            out = self.fc(out)
+            return out
